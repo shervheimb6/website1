88307ad1748ce1c1e7ff659ebcfa8216e1963206
shervheimb6 <shervheimb6@gmail.com>
2020-02-14 18:20:28 -0600

    fix

--- script1.js
+++ script1.js
@@ -2,24 +2,13 @@
 let element = document.querySelector("button");
 
 function turnButtonRed (){
-	element.style.backgroundColor = "red";
+  element.style.backgroundColor = "red";
   element.style.color = "white";
   element.innerHTML = "This is your surprise, the button changed!"; 
   
 }
 
 element.onclick = turnButtonRed;
-/*document.querySelector('button').innerHTML = 'Click me now!!! JK, dont!!!';*/
 
-/*function myFunction() {
-    var change = document.getElementById("toggle");
-    if (change.innerHTML === "on")
-    {
-        change.innerHTML = "off";
-    }
-    else {
-        change.innerHTML = "on";
-    }
-}*/
 
 
--- script2.js
+++ script2.js
@@ -1,4 +1,9 @@
 let bbutton = document.getElementById('bbutton');
 view.innerHTML = 'tiny button';
 
+$(document).ready(()=> {
+    $('.login-button').on('click', () => {
+        $('.login-form').show();
+    })
+})
 
